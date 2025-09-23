f0923b27a57588465cfbdeee3128a8a4322623d3
Kylie Sanchez <kyliemsanchez03@gmail.com>
2025-09-23 19:48:15 +0000

    Created bookings app and created the Movie, Seat, and Booking models

---
+++ homework2/movie_theater_booking/bookings/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+#Movie Model
+class Movie(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.PositiveIntegerField(help_text = "Duration in minutes")
+
+    def __str__(self):
+        return self.title
+    
+#Seat model
+class Seat(models.Model):
+    seat_number = models.CharField(max_length = 10, unique = True)
+    booking_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Seat {self.seat_number} - {'Booked' if self.booking_status == True else "Available"}"
+
+#Booking model
+class Booking(models.Model):
+    movie = models.ForeignKey(Movie, on_delete = models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
+    booking_date = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f"{self.user.username} booked {self.seat} for {self.movie}"
+
+
+
+
