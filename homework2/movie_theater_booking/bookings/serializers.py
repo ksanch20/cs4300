6d246ded7fb0f0ace884f3648a084f98419df4d3
Kylie Sanchez <kyliemsanchez03@gmail.com>
2025-09-26 21:48:49 +0000

    Create viewsets for Movie, Seat, and Booking

--- homework2/movie_theater_booking/bookings/serializers.py
+++ homework2/movie_theater_booking/bookings/serializers.py
@@ -1,16 +1,19 @@
 from rest_framework import serializers 
 from bookings.models import Movie, Seat, Booking
 
+#Serializer for Movie model
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie 
         fields = ['title', 'description', 'release_date', 'duration']
 
+#Serializer for Seat model
 class SeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seat
         fields = ['seat_number', 'booking_status']
 
+#Serializer for Booking
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
--- homework2/movie_theater_booking/bookings/views.py
+++ homework2/movie_theater_booking/bookings/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 from bookings.models import Movie, Seat, Booking
+from bookings.serializers import MovieSerializer, SeatSerializer, BookingSerializer
+from rest_framework import viewsets
 
 
 # Create your views here.
 
-def MovieViewSet(){
-
-
+#MovieViewSet for CRUD operations on movies
+def MovieViewSet(viewsets.ModelViewSet){
+   
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
 }
 
+#SeatViewSet for seat availability and booking 
 def SeatViewSet(){
-
-
+    serializer_class = SeatSerializer
+    queryset = Seat.objects.all()
 }
 
+#BookingViewSet for users to book seats and view their booking history
 def BookingViewSet(){
-
-
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
 }
 
 
