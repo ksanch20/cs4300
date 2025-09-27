92f3e8ae19a12fc864bea13d84c84bbab73aaf0d
Kylie Sanchez <kyliemsanchez03@gmail.com>
2025-09-27 01:02:31 +0000

    Register models in admin.py

--- homework2/movie_theater_booking/bookings/admin.py
+++ homework2/movie_theater_booking/bookings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Movie, Seat, Booking  # import models
 
 # Register your models here.
+
+# Register models to show them in admin
+admin.site.register(Movie)
+admin.site.register(Seat)
+admin.site.register(Booking)
