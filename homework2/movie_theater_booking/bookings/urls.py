a8401d44e6724ad3c3727fe6023aa3a405ae731c
Kylie Sanchez <kyliemsanchez03@gmail.com>
2025-09-27 19:53:01 +0000

    fix urls

--- homework2/movie_theater_booking/bookings/urls.py
+++ homework2/movie_theater_booking/bookings/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     #Movie list page showing available movies
-    path('movies/', views.movie_list, name='movies-list'),
+    path('movies/', views.movie_list, name='movies_list'),
 
     #Seat booking page for specific movie
     path('book-seat/<int:movie_id>/', views.book_seat, name='book_seat'),
 
     #Booking history page 
-    path('bookings/', views.booking_history, name='bookings_history'),
+    path('bookings/', views.booking_history, name='booking_history'),
 
     path('accounts/', include('django.contrib.auth.urls')),
 
