e5ecf8d5d4da6226f29783b05702cf3d8ce3fa14
Kylie Sanchez <kyliemsanchez03@gmail.com>
2025-09-28 06:02:32 +0000

    update settings for deployment

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cue=+wh6wa*$o96#gg$f2m*v=#(s0kab6rebio&x)61(oijpu9'
+import os
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
